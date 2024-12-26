c49a85e27f1f2865a69cdc76acda42ddd0d77359
BorodinAV <BorodinAV@319D-7>
2024-12-26 11:12:16 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace WpfAppLists
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var products = new List<UserControl1.Product>
+            {
+                new UserControl1.Product
+                {
+                    Name = "Ноутбук",
+                    Description = "Высокопроизводительный ноутбук для работы и игр",
+                    Manufacturer = "Dell",
+                    Price = 85000,
+                    Stock = 5,
+                    ImagePath = "Images/laptop.png"
+                },
+                new UserControl1.Product
+                {
+                    Name = "Смартфон",
+                    Description = "Смартфон с отличной камерой и большим экраном",
+                    Manufacturer = "Samsung",
+                    Price = 55000,
+                    Stock = 8,
+                    ImagePath = "Images/phone.png"
+                },
+                new UserControl1.Product
+                {
+                    Name = "Наушники",
+                    Description = "Беспроводные наушники с шумоподавлением",
+                    Manufacturer = "Sony",
+                    Price = 12000,
+                    Stock = 15,
+                    ImagePath = "Images/headphones.png"
+                }
+            };
+
+            ProductList.ItemsSource = products;
+        }
+    }
+}
---
+++ UserControl66.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace WpfAppLists
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Manufacturer { get; set; }
+            public double Price { get; set; }
+            public int Stock { get; set; }
+            public string ImagePath { get; set; }
+        }
+    }
+}
